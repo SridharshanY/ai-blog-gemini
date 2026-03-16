e80b4e4cab7cc6eb27f3e603a544446068fcbad2
SridharshanY <sridharshany.2001@gmail.com>
2026-03-16 12:22:52 +0530

    changed port

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json({ limit: '3mb' }));
 app.use('/api', apiRoutes);
 app.use('/api/auth', authRoutes)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const MONGO = process.env.MONGO_URI
 
 mongoose.connect(MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
